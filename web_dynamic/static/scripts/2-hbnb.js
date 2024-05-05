49a3faf17ffa275f68e4dee71102fad2ef18744e
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-05 15:00:18 +0300

    Task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+  handleAmenitiesCheckBoxes();
+
+  getAppStatus();
+});
+
+function handleAmenitiesCheckBoxes () {
   const amenities = {};
   $('.amenity-checkbox').change(onChange);
 
@@ -18,4 +24,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const text = Object.keys(amenities).sort().join(', ');
     h4.text(text);
   }
-});
+}
+
+function getAppStatus () {
+  const apiStatus = $('div#api_status');
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+    if (data.status === 'OK') {
+      apiStatus.addClass('available');
+    } else {
+      apiStatus.removeClass('available');
+    }
+  });
+}
